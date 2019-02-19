84030c5e02906d6db94c592eebe522b90752d0ee
pafable <39135798+pafable@users.noreply.github.com>
2019-02-18 19:45:29 -0500

    changed text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <p>oh hello world!</p>
+          <p>I was changed</p>
           <a
             className="App-link"
             href="https://reactjs.org"
