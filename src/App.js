ef8a1027c7b2f092ee60034f92e909674e1a9586
pafable <39135798+pafable@users.noreply.github.com>
2019-02-18 19:33:06 -0500

    changed welcome phrase

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <p>Yo breh</p>
+          <p>oh hello world!</p>
           <a
             className="App-link"
             href="https://reactjs.org"
